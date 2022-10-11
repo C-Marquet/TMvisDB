b7f918a102563cf7128e796502c7860a8ae273b0
C-Marquet <73125710+C-Marquet@users.noreply.github.com>
2022-10-11 18:35:47 +0200

    Update overview.py

--- utils/overview.py
+++ utils/overview.py
@@ -3,15 +3,16 @@ import streamlit as st
 def intro():
     st.markdown(
         "**TMvisDB** provides per-residue transmembrane topology annotations for all proteins in [AlphaFold DB](http://example.com) (~ 200 million proteins) "
-        "predicted as transmembrane proteins (~ 40 million). "
+        "predicted as transmembrane proteins (~ 46 million). "
         "The annotations are predicted with [TMbed](http://example.com), and are visualized by overlaying them with AlphaFold 2 structures.  \n")
 
     st.markdown(
         "###### :bulb: How to browse TMvisDB: \n"
-        "To browse the 40 million predicted transmembrane proteins in TMvisDB, you can show a random selection or use the following filters:  \n"
+        "To browse the 46 million predicted transmembrane proteins in TMvisDB via a table, you can show a random selection or use the following filters:  \n"
         "- Transmembrane topology (alpha-helix, beta-strand)  \n"
         "- Include/Exclude sequences with predicted signal peptides  \n"
-        "- Taxonomy (Domain, Kingdom)  \n")
+        "- Taxonomy (UniProt Organism Identifier, Domain, Kingdom)  \n"
+        "- Protein length  \n")
     st.markdown(
         "###### :bulb: How to visualize predicted transmembrane proteins: \n"
         "Single proteins of TMvisDB can be selected for 3D-visualization of per-residue transmembrane topology annotation. "
