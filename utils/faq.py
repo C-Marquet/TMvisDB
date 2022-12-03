04913f594e90531ae6e588b8dc92271059e4f864
C-Marquet <73125710+C-Marquet@users.noreply.github.com>
2022-12-03 11:47:31 +0100

    Update faq.py

--- utils/faq.py
+++ utils/faq.py
@@ -7,8 +7,8 @@ def quest():
 
     with st.expander("Why can't I find this protein I am interested in?"):
         st.markdown(
-        "(1) If the protein you are looking for is not part of AFDB, f.e. due to length restrictions, it is also not included in TMvis-DB.  \n"
-        "(2) The protein you are looking for might be membrane associated. TMvis-DB only contains proteins predicted to fully span a membrane.  \n"
+        "(1) If the protein you are looking for is not part of AFDB, f.e. due to length restrictions, it is also not included in TMvisDB.  \n"
+        "(2) The protein you are looking for might be membrane associated. TMvisDB only contains proteins predicted to fully span a membrane.  \n"
         "(3) While TMbed has a low false negative rate, some proteins are likely missing from the database.  \n"
         )
 
