602357fcf066b6d21c6b83c65d81b56e5cb9ff25
marquetce <112940337+marquetce@users.noreply.github.com>
2023-01-02 13:37:07 +0100

    Update overview.py

--- utils/overview.py
+++ utils/overview.py
@@ -12,7 +12,8 @@ def intro():
         "- Transmembrane topology (alpha-helix, beta-strand)  \n"
         "- Include/Exclude sequences with predicted signal peptides  \n"
         "- Taxonomy (UniProt Organism Identifier, Domain, Kingdom)  \n"
-        "- Protein length  \n")
+        "- Protein length  \n"
+        "Note: We follow followed general length restrictions of AlphaFold DB: minimum 16 amino acids and maximum 1,280 amino acids for all organisms except SwissProt (2,700 amino acids) and human (none).  \n")
     st.markdown(
         "###### :bulb: How to visualize predicted transmembrane proteins: \n"
         "Single proteins of TMvisDB can be selected for 3D-visualization of per-residue transmembrane topology annotation. "
